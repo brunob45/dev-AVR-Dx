58e40e48a1a7483ff446e8cc4b35a4bc81b4d5e9
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2021-01-24 15:39:41 -0500

    use dxcore

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,16 @@
-#include <avr/io.h>
-#include <util/delay.h>
+#include <Arduino.h>
 
-int main()
+void setup()
 {
-    _PROTECTED_WRITE(CLKCTRL_OSCHFCTRLA, CLKCTRL_FREQSEL_24M_gc);
+    pinMode(PIN_PC7, INPUT_PULLUP);
+    pinMode(PIN_PC6, OUTPUT);
+}
 
-    PORTC.DIRCLR = PIN7_bm;
-    PORTC.DIRSET = PIN6_bm;
-    PORTC.PIN7CTRL |= PORT_PULLUPEN_bm; 
-
-    while (1)
+void loop()
+{
+    if (digitalReadFast(PIN_PC7))
     {
-        if (PORTC.IN & (PIN7_bm))
-        {
-            PORTC.OUTTGL = PIN6_bm;
-            _delay_ms(500);
-        }
+        digitalWriteFast(PIN_PC6, CHANGE);
+        delay(500);
     }
 }
