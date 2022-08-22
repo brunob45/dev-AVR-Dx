396d2ff0a4df712016aaaa4557afc78b487264b6
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2022-08-22 00:08:46 -0400

    add Serial loopback

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,26 @@
 
 void setup()
 {
+    Serial.begin(115200);
+
     pinMode(PIN_PC7, INPUT_PULLUP);
     pinMode(PIN_PC6, OUTPUT);
 }
 
+static uint32_t last_update;
+
 void loop()
 {
-    if (digitalReadFast(PIN_PC7))
+    if (millis() - last_update > 500)
     {
-        digitalWriteFast(PIN_PC6, CHANGE);
-        delay(500);
+        last_update = millis();
+        if (digitalReadFast(PIN_PC7))
+        {
+            digitalWriteFast(PIN_PC6, CHANGE);
+        }
+    }
+    if (Serial.available())
+    {
+        Serial.write(Serial.read());
     }
 }
